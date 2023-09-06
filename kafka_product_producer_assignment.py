960f7c4e72cc95994be3a9281de03a2182f3bb7a
kakashi9359 <144196532+kakashi9359@users.noreply.github.com>
2023-09-06 09:57:34 +0530

    removed credentials

--- kafka_product_producer_assignment.py
+++ kafka_product_producer_assignment.py
@@ -18,17 +18,17 @@ def delivery_report(err,msg):
 
 # Define Kafka configuration
 kafka_config = {
-    'bootstrap.servers': 'pkc-41p56.asia-south1.gcp.confluent.cloud:9092',
+    'bootstrap.servers': '',
     'sasl.mechanisms': 'PLAIN',
     'security.protocol': 'SASL_SSL',
-    'sasl.username': 'VDGKT22VE2X765XB',
-    'sasl.password': '0NP2EBHvffUFoBtguP9QifgT24PrFq5NzBthGwlnmWJSB2z8nVeEL7oY2PJSokcf'
+    'sasl.username': '',
+    'sasl.password': ''
 }
 
 # Create a Schema Registry client
 schema_registry_client = SchemaRegistryClient({
   'url': 'https://psrc-l622j.us-east-2.aws.confluent.cloud',
-  'basic.auth.user.info': '{}:{}'.format('GNEQ6SUQCNPAWUUM', 'SGHFgMPb2U5HbuE51NjZlge8SeFw6cTegw2NGPet7qHbBYF+fmgSz6TTjkxoT4Vy')
+  'basic.auth.user.info': '{}:{}'.format('', '')
 })
 
 # Fetch the latest Avro schema for the value
